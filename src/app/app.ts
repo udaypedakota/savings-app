25ce2c7ab780c0c74d717414c4780055f460fecd
udaypedakota <peddakotaudaykumar@gmail.com>
2026-05-11 11:04:24 +0530

    fix: replace 7-item bottom nav with 5-item nav + More drawer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  moreOpen = false;
+}
